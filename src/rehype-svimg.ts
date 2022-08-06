b9dccc3e24a845d94c390b3b05d727207b3f7581
Chris Han <christopher.f.han@gmail.com>
2022-08-05 22:03:56 -0500

    Use type imports

--- src/rehype-svimg.ts
+++ src/rehype-svimg.ts
@@ -1,116 +1,122 @@
-import { Transformer } from 'unified';
+import type { Transformer } from 'unified';
 import visit from 'unist-util-visit';
-import { Node } from 'unist';
+import type { Node } from 'unist';
 import { Queue, generateComponentAttributes } from 'svimg/dist/process';
 
 export interface RehypeSvimgOptions {
-    inputDir: string;
-    outputDir: string;
-    webp?: boolean;
-    avif?: boolean;
-    width?: number;
-    blur?: number;
-    quality?: number;
-    generateImages?: boolean;
-    srcPrefix?: string;
-    skip?: (options: { src: string; }) => boolean;
+  inputDir: string;
+  outputDir: string;
+  webp?: boolean;
+  avif?: boolean;
+  width?: number;
+  blur?: number;
+  quality?: number;
+  generateImages?: boolean;
+  srcPrefix?: string;
+  skip?: (options: { src: string }) => boolean;
 }
 
 interface ImageNode {
-    type: 'element';
-    tagName: string;
-    properties: {
-        [attr: string]: string;
-    },
+  type: 'element';
+  tagName: string;
+  properties: {
+    [attr: string]: string;
+  };
 }
 
 const TAG_NAME = 's-image';
 
-function getIntOption(props: { [attr: string]: string }, options: RehypeSvimgOptions, prop: 'width' | 'quality'): number | undefined {
-    const propVal = props[prop];
+function getIntOption(
+  props: { [attr: string]: string },
+  options: RehypeSvimgOptions,
+  prop: 'width' | 'quality',
+): number | undefined {
+  const propVal = props[prop];
 
-    if (propVal) {
-        if (/^[0-9]+$/.test(propVal)) {
-            return parseInt(propVal, 10);
-        }
-    } else if (options && options[prop]) {
-        return options[prop];
+  if (propVal) {
+    if (/^[0-9]+$/.test(propVal)) {
+      return parseInt(propVal, 10);
     }
+  } else if (options && options[prop]) {
+    return options[prop];
+  }
 
-    return undefined;
+  return undefined;
 }
 
 export default function rehypeSvimg(options?: RehypeSvimgOptions): Transformer {
+  if (!options?.inputDir) {
+    throw new Error('Input dir is required');
+  }
+  if (!options?.outputDir) {
+    throw new Error('Output dir is required');
+  }
 
-    if (!options?.inputDir) {
-        throw new Error('Input dir is required');
+  const queue = new Queue();
+
+  return async function transformer(tree, file): Promise<Node> {
+    const imageNodes: ImageNode[] = [];
+
+    visit(tree, { type: 'element', tagName: 'img' }, (node: Node) => {
+      imageNodes.push(node as any as ImageNode);
+    });
+
+    if (options.srcPrefix && !options.srcPrefix.endsWith('/')) {
+      options.srcPrefix += '/';
     }
-    if (!options?.outputDir) {
-        throw new Error('Output dir is required');
-    }
 
-    const queue = new Queue();
-
-    return async function transformer(tree, file): Promise<Node> {
-
-        const imageNodes: ImageNode[] = [];
-
-        visit(tree, { type: 'element', tagName: 'img' }, (node: Node) => {
-            imageNodes.push(node as any as ImageNode);
-        });
-
-        if (options.srcPrefix && !options.srcPrefix.endsWith('/')) {
-            options.srcPrefix += '/';
+    await Promise.all(
+      imageNodes.map(async (node) => {
+        if (!(node.properties && node.properties.src)) {
+          return;
         }
 
-        await Promise.all(imageNodes.map(async (node) => {
-            if (!(node.properties && node.properties.src)) {
-                return;
-            }
+        const width = getIntOption(node.properties, options, 'width');
+        const quality = getIntOption(node.properties, options, 'quality');
 
-            const width = getIntOption(node.properties, options, 'width');
-            const quality = getIntOption(node.properties, options, 'quality');
+        let src = node.properties.src;
+        if (options.srcPrefix) {
+          src = options.srcPrefix + src;
+        }
 
-            let src = node.properties.src;
-            if (options.srcPrefix) {
-                src = options.srcPrefix + src;
-            }
+        if (options.skip && options.skip({ src })) {
+          return;
+        }
 
-            if (options.skip && options.skip({ src })) {
-                return;
-            }
+        const immediate =
+          node.properties.immediate === '' ||
+          node.properties.immediate === 'true';
 
-            const immediate = node.properties.immediate === '' || node.properties.immediate === 'true';
+        const attributes = await generateComponentAttributes({
+          src,
+          queue,
+          inputDir: options.inputDir,
+          outputDir: options.outputDir,
+          webp: options.webp,
+          avif: options.avif,
+          widths: width ? [width] : undefined,
+          quality,
+          skipGeneration: !options?.generateImages,
+          skipPlaceholder: immediate || undefined,
+        });
 
-            const attributes = await generateComponentAttributes({
-                src,
-                queue,
-                inputDir: options.inputDir,
-                outputDir: options.outputDir,
-                webp: options.webp,
-                avif: options.avif,
-                widths: width ? [width] : undefined,
-                quality,
-                skipGeneration: !(options?.generateImages),
-                skipPlaceholder: immediate || undefined,
-            });
+        Object.assign(node.properties, attributes);
+        if (src !== node.properties.src) {
+          node.properties.src = src;
+        }
+        if (options?.width && !node.properties.width) {
+          node.properties.width = options.width.toString();
+        }
+        if (options?.blur && !node.properties.blur) {
+          node.properties.blur = options.blur.toString();
+        }
+        if (options?.quality && !node.properties.quality) {
+          node.properties.quality = options.quality.toString();
+        }
+        node.tagName = TAG_NAME;
+      }),
+    );
 
-            Object.assign(node.properties, attributes);
-            if (src !== node.properties.src) {
-                node.properties.src = src;
-            }
-            if (options?.width && !node.properties.width) {
-                node.properties.width = options.width.toString();
-            }
-            if (options?.blur && !node.properties.blur) {
-                node.properties.blur = options.blur.toString();
-            }
-            if (options?.quality && !node.properties.quality) {
-                node.properties.quality = options.quality.toString();
-            }
-            node.tagName = TAG_NAME;
-        }));
-
-        return tree;
-    }
+    return tree;
+  };
 }
